aafc0d769777a701b8302c2caa7843a20c92c2e0
zainabzoaib <zainab.zoaib@gmail.com>
2017-03-29 18:57:30 +0500

    Local Settings

---
+++ 1.0.5/logs/log-2017-03-29.php
@@ -0,0 +1,213 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2017-03-29 13:54:11 --> Config Class Initialized
+INFO - 2017-03-29 13:54:11 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:54:11 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:54:11 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:54:11 --> URI Class Initialized
+DEBUG - 2017-03-29 13:54:11 --> No URI present. Default controller set.
+INFO - 2017-03-29 13:54:11 --> Router Class Initialized
+INFO - 2017-03-29 13:54:11 --> Output Class Initialized
+INFO - 2017-03-29 13:54:11 --> Security Class Initialized
+DEBUG - 2017-03-29 13:54:11 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:54:11 --> Input Class Initialized
+INFO - 2017-03-29 13:54:11 --> Language Class Initialized
+INFO - 2017-03-29 13:54:11 --> Language Class Initialized
+INFO - 2017-03-29 13:54:11 --> Config Class Initialized
+INFO - 2017-03-29 13:54:11 --> Loader Class Initialized
+INFO - 2017-03-29 13:54:11 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:54:11 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:54:11 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:54:15 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:54:15 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:54:15 --> Email Class Initialized
+INFO - 2017-03-29 13:54:15 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:54:15 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:54:15 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:54:15 --> Model Class Initialized
+INFO - 2017-03-29 13:54:15 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:54:15 --> Controller Class Initialized
+DEBUG - 2017-03-29 13:54:15 --> Dashboard MX_Controller Initialized
+INFO - 2017-03-29 13:55:48 --> Config Class Initialized
+INFO - 2017-03-29 13:55:48 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:55:48 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:55:48 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:55:48 --> URI Class Initialized
+DEBUG - 2017-03-29 13:55:48 --> No URI present. Default controller set.
+INFO - 2017-03-29 13:55:48 --> Router Class Initialized
+INFO - 2017-03-29 13:55:48 --> Output Class Initialized
+INFO - 2017-03-29 13:55:48 --> Security Class Initialized
+DEBUG - 2017-03-29 13:55:48 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:55:48 --> Input Class Initialized
+INFO - 2017-03-29 13:55:48 --> Language Class Initialized
+INFO - 2017-03-29 13:55:48 --> Language Class Initialized
+INFO - 2017-03-29 13:55:48 --> Config Class Initialized
+INFO - 2017-03-29 13:55:48 --> Loader Class Initialized
+INFO - 2017-03-29 13:55:48 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:55:48 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:55:48 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:55:48 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:55:48 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:55:48 --> Email Class Initialized
+INFO - 2017-03-29 13:55:48 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:55:48 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:55:48 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:55:48 --> Model Class Initialized
+INFO - 2017-03-29 13:55:48 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:55:48 --> Controller Class Initialized
+DEBUG - 2017-03-29 13:55:48 --> Dashboard MX_Controller Initialized
+INFO - 2017-03-29 13:56:06 --> Config Class Initialized
+INFO - 2017-03-29 13:56:06 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:56:06 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:56:06 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:56:06 --> URI Class Initialized
+DEBUG - 2017-03-29 13:56:06 --> No URI present. Default controller set.
+INFO - 2017-03-29 13:56:06 --> Router Class Initialized
+INFO - 2017-03-29 13:56:06 --> Output Class Initialized
+INFO - 2017-03-29 13:56:06 --> Security Class Initialized
+DEBUG - 2017-03-29 13:56:06 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:56:06 --> Input Class Initialized
+INFO - 2017-03-29 13:56:06 --> Language Class Initialized
+INFO - 2017-03-29 13:56:06 --> Language Class Initialized
+INFO - 2017-03-29 13:56:06 --> Config Class Initialized
+INFO - 2017-03-29 13:56:06 --> Loader Class Initialized
+INFO - 2017-03-29 13:56:06 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:56:06 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:56:06 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:56:06 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:56:06 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:56:06 --> Email Class Initialized
+INFO - 2017-03-29 13:56:06 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:56:06 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:56:06 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:56:06 --> Model Class Initialized
+INFO - 2017-03-29 13:56:06 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:56:06 --> Controller Class Initialized
+DEBUG - 2017-03-29 13:56:06 --> Dashboard MX_Controller Initialized
+INFO - 2017-03-29 13:56:06 --> Config Class Initialized
+INFO - 2017-03-29 13:56:06 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:56:06 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:56:06 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:56:06 --> URI Class Initialized
+INFO - 2017-03-29 13:56:06 --> Router Class Initialized
+INFO - 2017-03-29 13:56:06 --> Output Class Initialized
+INFO - 2017-03-29 13:56:06 --> Security Class Initialized
+DEBUG - 2017-03-29 13:56:06 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:56:06 --> Input Class Initialized
+INFO - 2017-03-29 13:56:06 --> Language Class Initialized
+INFO - 2017-03-29 13:56:07 --> Language Class Initialized
+INFO - 2017-03-29 13:56:07 --> Config Class Initialized
+INFO - 2017-03-29 13:56:07 --> Loader Class Initialized
+INFO - 2017-03-29 13:56:07 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:56:07 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:56:07 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:56:07 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:56:07 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:56:07 --> Email Class Initialized
+INFO - 2017-03-29 13:56:07 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:56:07 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:56:07 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:56:07 --> Model Class Initialized
+INFO - 2017-03-29 13:56:07 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:56:07 --> Controller Class Initialized
+INFO - 2017-03-29 13:56:07 --> Helper loaded: form_helper
+INFO - 2017-03-29 13:56:07 --> Form Validation Class Initialized
+INFO - 2017-03-29 13:56:07 --> Language file loaded: language/english/auth_lang.php
+DEBUG - 2017-03-29 13:56:08 --> File loaded: D:\wamp64\www\FarnekHRMS\1.0.5\views\auth/login.php
+DEBUG - 2017-03-29 13:56:08 --> File loaded: D:\wamp64\www\FarnekHRMS\1.0.5\views\themes/login.php
+INFO - 2017-03-29 13:56:08 --> Final output sent to browser
+DEBUG - 2017-03-29 13:56:08 --> Total execution time: 1.3611
+INFO - 2017-03-29 13:56:33 --> Config Class Initialized
+INFO - 2017-03-29 13:56:33 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:56:33 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:56:33 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:56:33 --> URI Class Initialized
+INFO - 2017-03-29 13:56:33 --> Router Class Initialized
+INFO - 2017-03-29 13:56:33 --> Output Class Initialized
+INFO - 2017-03-29 13:56:33 --> Security Class Initialized
+DEBUG - 2017-03-29 13:56:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:56:33 --> Input Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language Class Initialized
+INFO - 2017-03-29 13:56:33 --> Config Class Initialized
+INFO - 2017-03-29 13:56:33 --> Loader Class Initialized
+INFO - 2017-03-29 13:56:33 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:56:33 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:56:33 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:56:33 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:56:33 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:56:33 --> Email Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:56:33 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:56:33 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:56:33 --> Model Class Initialized
+INFO - 2017-03-29 13:56:33 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:56:33 --> Controller Class Initialized
+INFO - 2017-03-29 13:56:33 --> Helper loaded: form_helper
+INFO - 2017-03-29 13:56:33 --> Form Validation Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language file loaded: language/english/auth_lang.php
+INFO - 2017-03-29 13:56:33 --> Language file loaded: language/english/form_validation_lang.php
+INFO - 2017-03-29 13:56:33 --> Config Class Initialized
+INFO - 2017-03-29 13:56:33 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:56:33 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:56:33 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:56:33 --> URI Class Initialized
+DEBUG - 2017-03-29 13:56:33 --> No URI present. Default controller set.
+INFO - 2017-03-29 13:56:33 --> Router Class Initialized
+INFO - 2017-03-29 13:56:33 --> Output Class Initialized
+INFO - 2017-03-29 13:56:33 --> Security Class Initialized
+DEBUG - 2017-03-29 13:56:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:56:33 --> Input Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language Class Initialized
+INFO - 2017-03-29 13:56:33 --> Config Class Initialized
+INFO - 2017-03-29 13:56:33 --> Loader Class Initialized
+INFO - 2017-03-29 13:56:33 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:56:33 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:56:33 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:56:33 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:56:33 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:56:33 --> Email Class Initialized
+INFO - 2017-03-29 13:56:33 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:56:33 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:56:33 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:56:33 --> Model Class Initialized
+INFO - 2017-03-29 13:56:33 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:56:33 --> Controller Class Initialized
+DEBUG - 2017-03-29 13:56:33 --> Dashboard MX_Controller Initialized
+DEBUG - 2017-03-29 13:56:34 --> File loaded: D:\wamp64\www\FarnekHRMS\1.0.5\views\Dashboard/Dashboard.php
+ERROR - 2017-03-29 13:56:34 --> Module controller failed to run: Notifications/Notifications/widget
+DEBUG - 2017-03-29 13:56:34 --> File loaded: D:\wamp64\www\FarnekHRMS\1.0.5\views\themes/superhero.php
+INFO - 2017-03-29 13:56:34 --> Final output sent to browser
+DEBUG - 2017-03-29 13:56:34 --> Total execution time: 0.2640
+INFO - 2017-03-29 13:56:42 --> Config Class Initialized
+INFO - 2017-03-29 13:56:42 --> Hooks Class Initialized
+DEBUG - 2017-03-29 13:56:42 --> UTF-8 Support Enabled
+INFO - 2017-03-29 13:56:42 --> Utf8 Class Initialized
+INFO - 2017-03-29 13:56:42 --> URI Class Initialized
+INFO - 2017-03-29 13:56:42 --> Router Class Initialized
+INFO - 2017-03-29 13:56:42 --> Output Class Initialized
+INFO - 2017-03-29 13:56:42 --> Security Class Initialized
+DEBUG - 2017-03-29 13:56:42 --> Global POST, GET and COOKIE data sanitized
+INFO - 2017-03-29 13:56:42 --> Input Class Initialized
+INFO - 2017-03-29 13:56:42 --> Language Class Initialized
+INFO - 2017-03-29 13:56:42 --> Language Class Initialized
+INFO - 2017-03-29 13:56:42 --> Config Class Initialized
+INFO - 2017-03-29 13:56:42 --> Loader Class Initialized
+INFO - 2017-03-29 13:56:42 --> Helper loaded: url_helper
+INFO - 2017-03-29 13:56:42 --> Helper loaded: html_helper
+INFO - 2017-03-29 13:56:42 --> Database Driver Class Initialized
+INFO - 2017-03-29 13:56:42 --> Session: Class initialized using 'files' driver.
+DEBUG - 2017-03-29 13:56:42 --> Config file loaded: D:\wamp64\www\FarnekHRMS\1.0.5\config/ion_auth.php
+INFO - 2017-03-29 13:56:42 --> Email Class Initialized
+INFO - 2017-03-29 13:56:42 --> Language file loaded: language/english/ion_auth_lang.php
+INFO - 2017-03-29 13:56:42 --> Helper loaded: cookie_helper
+INFO - 2017-03-29 13:56:42 --> Helper loaded: language_helper
+INFO - 2017-03-29 13:56:42 --> Model Class Initialized
+INFO - 2017-03-29 13:56:42 --> Helper loaded: date_helper
+INFO - 2017-03-29 13:56:42 --> Controller Class Initialized
+DEBUG - 2017-03-29 13:56:42 --> File loaded: D:\wamp64\www\FarnekHRMS\1.0.5\modules/Appraisal/views/ScoreWorkerAppraisal.php
+ERROR - 2017-03-29 13:56:42 --> Module controller failed to run: Notifications/Notifications/widget
+DEBUG - 2017-03-29 13:56:42 --> File loaded: D:\wamp64\www\FarnekHRMS\1.0.5\views\themes/superhero.php
+INFO - 2017-03-29 13:56:42 --> Final output sent to browser
+DEBUG - 2017-03-29 13:56:42 --> Total execution time: 0.1660
